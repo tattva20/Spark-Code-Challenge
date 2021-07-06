87df41b04688aa5cd0e26c807a62e70b43fa2b06
Octavio Rojas <orojas@carnival.com>
2021-07-06 10:28:20 -0500

    Initial Commit

---
+++ Spark Code Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spark Code Challenge
+//
+//  Created by Octavio Rojas on 06/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
